66bf88e12d257b6f332b7f127bad234cb818b4b3
Cemous <tang_liangliang@yeah.net>
2015-08-19 11:18:28 +0800

    Hierapolis
    初始模板

--- MyTrunk/MyTrunk.Web/Startup.cs
+++ MyTrunk/MyTrunk.Web/Startup.cs
@@ -1,14 +1,15 @@
 ﻿using Microsoft.Owin;
 using Owin;
 
-[assembly: OwinStartupAttribute(typeof(MyTrunk.Web.Startup))]
+[assembly: OwinStartup(typeof(MyTrunk.Web.Startup))]
+
 namespace MyTrunk.Web
 {
-    public partial class Startup
+    public class Startup
     {
         public void Configuration(IAppBuilder app)
         {
-            ConfigureAuth(app);
+            // 有关如何配置应用程序的详细信息，请访问 http://go.microsoft.com/fwlink/?LinkID=316888
         }
     }
 }
